a0a2ffdec2786b90a06a6f5f3602cd240c50a8b1
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-10-15 18:56:01 -0700

    First commit

---
+++ src/Features/feature_engineering.py
@@ -0,0 +1,14 @@
+#Read in libraries
+import numpy as np
+import pandas as pd
+import sklearn
+
+class Dataframe(self, dataframe):
+    self.data = dataframe
+
+    def encoder(self)
+
+
+#Encode categorical variables using column transformer
+
+#Scale/Normalize variables
