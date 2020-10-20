5beb0ef3b8b43509fddb6817dbb5cd5e574ea103
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-10-19 18:54:48 -0700

    Added functions but we\'ll need to replace with a full pipeline

--- src/Features/feature_engineering.py
+++ src/Features/feature_engineering.py
@@ -1,14 +1,40 @@
 #Read in libraries
 import numpy as np
 import pandas as pd
-import sklearn
 
-class Dataframe(self, dataframe):
-    self.data = dataframe
+#Import all the sklearn goodies
+from sklearn.preprocessing import OneHotEncoder
 
-    def encoder(self)
+#Import the statsmodels goodies
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
 
+class Preprocessor:
+    def __init__(self, dataframe):
+        self.data = dataframe
 
 #Encode categorical variables using column transformer
 
+    def nominal_encoder(self, nominal_features, drop= None,
+                        categories = 'auto'):
+        encoder= OneHotEncoder(drop = drop, handle_unknown='ignore',
+                                categories= categories)
+        x = encoder.fit_transform(self.data[nominal_features])
+        return x
+        
+
+class Tests:
+    
+
+    def __init__(self, dataframe):
+        self.data = dataframe
+        
+    def variance_inflation_score(self):
+        
+        X= data.add_constant()
+#Encode categorical variables using column transformer
+
 #Scale/Normalize variables
+
+#Test for multicollinearity 
+#Test for normality
