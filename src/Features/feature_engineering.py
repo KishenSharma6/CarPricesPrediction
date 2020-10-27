fefcee0500ca6f9cf4ca1ec38b28f170e2c01ea3
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-10-26 17:50:32 -0700

    Added additional tests to test class and created Transformations class

--- src/Features/feature_engineering.py
+++ src/Features/feature_engineering.py
@@ -1,10 +1,13 @@
-#Read in libraries
+#Read in standard libraries
 import numpy as np
 import pandas as pd
 
 #Import the statsmodels goodies
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from statsmodels.tools.tools import add_constant
+
+#Import scipy goodies
+from scipy import stats
     
 
 class Tests:
@@ -23,6 +26,32 @@ class Tests:
         VIF= pd.Series([variance_inflation_factor(X.values, i) 
                         for i in range(X.shape[1])],index= X.columns).sort_values(ascending = False)
         return VIF
+
+    def skew_measurement(self):
+        return self.data.skew()
+
+    def shapiro_wilks(self,target):
+        shapiro_test = stats.shapiro(self.data[target])
+        print('Shapiro statistic: %s \nP-Value: %s' % (shapiro_test.statistic, shapiro_test.pvalue))
+
+
+class Transformations(Tests):
+    def __init__(self, data):
+        super().__init__(data)
+
+    def square_root(self, target):
+        return np.sqrt(self.data[target])
+
+    def cube_root(self, target):
+        return np.cbrt(self.data[target])
+
+    def log_transformation(self, target):
+        return np.log(self.data[target])
+    
+    def boxcox_transformation(self, target):
+        return stats.boxcox(self.data[target])
+
+
 #Encode categorical variables using column transformer
 
 #Scale/Normalize variables
