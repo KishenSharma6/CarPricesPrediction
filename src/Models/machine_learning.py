ac9f32fed6e4253cda3b58e97d8a1f581a53d3a7
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-10-27 18:59:39 -0700

    Added regression metrics functionality

--- src/Models/machine_learning.py
+++ src/Models/machine_learning.py
@@ -3,3 +3,23 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import r2_score
+
+class Linear_Regression:
+    def __init__(self, dataframe):
+        self.data = dataframe
+
+
+    def pipeline(self ):
+        pass
+
+
+def regression_metrics(predictions, actual):
+    errors = predictions - actual
+    mape = np.mean(np.abs(errors)/np.abs(actual)) * 100
+    mse = np.square(errors).mean()
+    rmse = np.sqrt(mse)
+    mae = np.abs(errors).mean()
+    r2 = r2_score(actual, predictions)
+    metrics = {'MAE': mae,'MAPE':mape,'MSE': mse, 'RMSE':rmse}
+    return(metrics)
